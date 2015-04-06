8747d7a07644f1499792b868663ec410513457ae
ashiver <anthony.lee.shiver@gmail.com>
2015-04-06 19:20:03 +0000

    Init commit

---
+++ tbay.py
@@ -0,0 +1,47 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
+from sqlalchemy import Column, Integer, String, DateTime, Float, ForeignKey
+from sqlalchemy.orm import relationship
+
+
+engine = create_engine('postgresql://action:action@localhost:5432/tbay')
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
+
+
+    
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    username = Column(String, nullable=False)
+    password = Column(String, nullable=False)
+    bids = relationship("Bid", backref="user")
+    auctions = relationship("Item", backref="user")
+
+    
+    
+class Item(Base):
+    __tablename__ = "items"
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    description = Column(String)
+    start_time = Column(DateTime, default=datetime.utcnow)
+    auctioner = Column(Integer, ForeignKey('users.id'), nullable=False)
+    bids = relationship("Bid", backref="item")
+    
+    
+
+    
+class Bid(Base):
+    __tablename__ = "bids"
+    id = Column(Integer, primary_key=True)
+    price = Column(Float, nullable=False)
+    bidder_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    item_id = Column(Integer, ForeignKey('items.id'), nullable=False)
+    
+
+    
+Base.metadata.create_all(engine)
